aca3a18bf8c91a4b09bb1918da5bd0c0eca5713a
Gabriel Fernandes <gabrielbfernandes@gmail.com>
2026-02-15 23:29:12 -0300

    First try

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,10 @@
-Hooks.once('init', async function() {
 
-});
 
-Hooks.once('ready', async function() {
-
+Hooks.on("initializeDynamicTokenRingConfig", (a) => {
+    const default_ring_rctr = new foundry.canvas.placeables.tokens.DynamicRingData({
+        Id: 'default_ring_rctr',
+        label: "Default ring",
+        spritesheet: "modules/narnia-private-module/assets/default ring/ring-dnd.json"
+    });
+    a.addConfig("default_ring_rctr", default_ring_rctr);
 });
